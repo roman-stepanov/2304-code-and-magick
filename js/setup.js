068375e2cb082ac3c9f97435f5ed464fd244e5c6
Roman Stepanov <romik112@yandex.ru>
2017-02-10 23:18:21 +0300

    создал функции обратного вызова

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,7 @@
 (function () {
 
   var setupWindow = document.querySelector('.setup');
-  var setupOpen = document.querySelector('.setup-open');
+  var setupOpen = document.querySelector('.setup-open-icon');
   var setupClose = setupWindow.querySelector('.setup-close');
   var setupUserName = setupWindow.querySelector('.setup-user-name');
 
@@ -42,34 +42,84 @@
     setupClose.addEventListener('keydown', closeSetupHandler);
     document.addEventListener('keydown', setupPressESC);
 
+    wizardCoat.addEventListener('click', changeColorCoat);
+    wizardCoat.addEventListener('keydown', changeColorCoat);
+    wizardEyes.addEventListener('click', changeColorEyes);
+    wizardEyes.addEventListener('keydown', changeColorEyes);
+    fireballWrap.addEventListener('click', changeColorFireball);
+    fireballWrap.addEventListener('keydown', changeColorFireball);
+
     setupOpen.setAttribute('aria-pressed', 'true');
   };
 
-  var closeSetup = function () {
+  var closeSetup = function (callback) {
     setupWindow.classList.add('invisible');
 
     setupClose.removeEventListener('click', closeSetupHandler);
     setupClose.removeEventListener('keydown', closeSetupHandler);
     document.removeEventListener('keydown', setupPressESC);
 
+    wizardCoat.removeEventListener('click', changeColorCoat);
+    wizardCoat.removeEventListener('keydown', changeColorCoat);
+    wizardEyes.removeEventListener('click', changeColorEyes);
+    wizardEyes.removeEventListener('keydown', changeColorEyes);
+    fireballWrap.removeEventListener('click', changeColorFireball);
+    fireballWrap.removeEventListener('keydown', changeColorFireball);
+
     setupOpen.setAttribute('aria-pressed', 'false');
+
+    if (typeof callback === 'function') {
+      callback();
+    }
+  };
+
+  var isActivationEvent = function (evt) {
+    return evt.type === 'click' || window.evtPressKey.isPressENTER(evt);
   };
 
   var showSetupHandler = function (evt) {
-    if (evt.type === 'click' || window.evtPressKey.isPressENTER(evt)) {
+    if (isActivationEvent(evt)) {
       showSetup();
     }
   };
 
+  var callbackCloseSetup = function (evt) {
+    if (evt.type === 'keydown') {
+      setupOpen.focus();
+    }
+  };
+
   var closeSetupHandler = function (evt) {
-    if (evt.type === 'click' || window.evtPressKey.isPressENTER(evt)) {
-      closeSetup();
+    if (isActivationEvent(evt)) {
+      closeSetup(callbackCloseSetup(evt));
     }
   };
 
   var setupPressESC = function (evt) {
     if (!setupWindow.classList.contains('invisible') && window.evtPressKey.isPressESC(evt)) {
-      closeSetup();
+      closeSetup(callbackCloseSetup(evt));
+    }
+  };
+
+  var callbackColorizeElement = function (element, color, property) {
+    element.style[property] = color;
+  };
+
+  var changeColorCoat = function (evt) {
+    if (isActivationEvent(evt)) {
+      window.colorizeElement(wizardCoat, colorCoat, 'fill', callbackColorizeElement);
+    }
+  };
+
+  var changeColorEyes = function (evt) {
+    if (isActivationEvent(evt)) {
+      window.colorizeElement(wizardEyes, colorEyes, 'fill', callbackColorizeElement);
+    }
+  };
+
+  var changeColorFireball = function (evt) {
+    if (isActivationEvent(evt)) {
+      window.colorizeElement(fireballWrap, colorFireball, 'backgroundColor', callbackColorizeElement);
     }
   };
 
@@ -81,7 +131,4 @@
   setupOpen.addEventListener('click', showSetupHandler);
   setupOpen.addEventListener('keydown', showSetupHandler);
   validationSetup();
-  window.colorizeElement(wizardCoat, colorCoat, 'fill');
-  window.colorizeElement(wizardEyes, colorEyes, 'fill');
-  window.colorizeElement(fireballWrap, colorFireball, 'backgroundColor');
 })();
