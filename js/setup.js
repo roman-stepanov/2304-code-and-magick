13f1f1e540f83cafe648a85a554236161703ee18
Roman Stepanov <romik112@yandex.ru>
2017-02-16 00:42:21 +0300

    Использовал XMLHttpRequest

--- js/setup.js
+++ js/setup.js
@@ -6,11 +6,15 @@
   var setupOpen = document.querySelector('.setup-open-icon');
   var setupClose = setupWindow.querySelector('.setup-close');
   var setupUserName = setupWindow.querySelector('.setup-user-name');
+  var setupSimilar = setupWindow.querySelector('.setup-similar');
 
   var wizard = document.querySelector('#wizard');
   var wizardCoat = wizard.querySelector('#wizard-coat');
   var wizardEyes = wizard.querySelector('#wizard-eyes');
   var fireballWrap = document.querySelector('.setup-fireball-wrap');
+
+  var WIZARDS_DATA = 'https://intensive-javascript-server-pedmyactpq.now.sh/code-and-magick/data';
+  var wizards = null;
 
   var colorCoat = [
     'rgb(101, 137, 164)',
@@ -35,7 +39,23 @@
     '#e6e848'
   ];
 
-  var showSetup = function () {
+  var drawSimilarWizards = function () {
+    var MAX_WIZARDS = 5;
+    var similarWizards = wizards.slice();
+    var fragment = document.createDocumentFragment();
+
+    while (similarWizards.length > MAX_WIZARDS) {
+      similarWizards.splice(Math.floor(Math.random() * similarWizards.length), 1);
+    }
+
+    setupSimilar.innerHTML = '';
+    similarWizards.forEach(function (item, i) {
+      fragment.appendChild(window.render(item));
+    });
+    setupSimilar.appendChild(fragment);
+  };
+
+  var showSetup = function (callback) {
     setupWindow.classList.remove('invisible');
 
     setupClose.addEventListener('click', closeSetupHandler);
@@ -50,6 +70,10 @@
     fireballWrap.addEventListener('keydown', changeColorFireball);
 
     setupOpen.setAttribute('aria-pressed', 'true');
+
+    if (typeof callback === 'function') {
+      callback();
+    }
   };
 
   var closeSetup = function (callback) {
@@ -77,9 +101,16 @@
     return evt.type === 'click' || window.evtPressKey.isPressENTER(evt);
   };
 
+  var callbackShowSetup = function () {
+    window.load(WIZARDS_DATA, function (data) {
+      wizards = JSON.parse(data);
+      drawSimilarWizards();
+    });
+  };
+
   var showSetupHandler = function (evt) {
     if (isActivationEvent(evt)) {
-      showSetup();
+      showSetup(callbackShowSetup);
     }
   };
 
@@ -103,6 +134,7 @@
 
   var callbackColorizeElement = function (element, color, property) {
     element.style[property] = color;
+    setTimeout(drawSimilarWizards, 5000);
   };
 
   var changeColorCoat = function (evt) {
